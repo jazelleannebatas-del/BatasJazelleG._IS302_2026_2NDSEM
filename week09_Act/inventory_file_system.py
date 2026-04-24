59165cac0a2a00972a820b60d304eda5478eacb6
jazelleannebatas-del <jazelleannebatas@gmail.com>
2026-04-23 21:58:16 -0400

    Delete week09_Act/inventory_file_system.py

--- week09_Act/inventory_file_system.py
+++
@@ -1,7 +0,0 @@
-product_jab = input("Enter product name: ")
-price_jab = input("Enter price: ")
-
-with open("inventory.txt", "a") as file:
-    file.write(product_jab + "," + price_jab + "\n")
-
-print("Product saved successfully")
