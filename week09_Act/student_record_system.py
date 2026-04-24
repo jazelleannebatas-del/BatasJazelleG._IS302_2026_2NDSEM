2664cdd80bc0b06b7a3489d08babbdbb506b01ad
jazelleannebatas-del <jazelleannebatas@gmail.com>
2026-04-23 21:58:46 -0400

    Delete week09_Act/student_record_system.py

--- week09_Act/student_record_system.py
+++
@@ -1,9 +0,0 @@
-name_jab = input("Enter student name: ")
-course_jab = input("Enter course: ")
-with open("students.txt", "a") as file_jab:
-    file_jab.write(name_jab + "," + course_jab + "\n")
-
-print("\nStudent Records")
-with open("students.txt", "r") as file_jab:
-    for line_jab in file_jab:
-        print(line_jab.strip())
