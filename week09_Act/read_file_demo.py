8547ef9a372b42993bc00eeaeb3c3904288f507f
jazelleannebatas-del <jazelleannebatas@gmail.com>
2026-04-23 22:07:49 -0400

    Add files via upload

---
+++ week09_Act/read_file_demo.py
@@ -0,0 +1,3 @@
+with open("data.txt", "r") as file_jab:
+    content_jab = file_jab.read()
+    print(content_jab)
---
+++ week09_Act/write_file_demo.py
@@ -0,0 +1,2 @@
+with open("data.txt", "w") as file_jab:
+    file_jab.write("Welcome to Python File Handling")
