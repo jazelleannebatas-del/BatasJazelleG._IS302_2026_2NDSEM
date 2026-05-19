a0847752fc6f35944cf3789ee0d395dba2b4a947
jazelleannebatas-del <jazelleannebatas@gmail.com>
2026-05-19 00:04:26 -0400

    Delete week10_act/input_validation.py

--- week10_act/input_validation.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        number_jab = int(input("Enter a number: "))
-        break
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-
-print("Valid number entered:", number_jab)
